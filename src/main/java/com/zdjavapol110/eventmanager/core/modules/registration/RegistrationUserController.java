137bf64fe2f4d2d07514dbe7f64cc71ebe628f28
Przemyslaw <brain667@gmail.com>
2022-05-22 14:56:30 +0200

    allowPublicKeyRetrieval=true

--- src/main/java/com/zdjavapol110/eventmanager/core/modules/registration/RegistrationUserController.java
+++ src/main/java/com/zdjavapol110/eventmanager/core/modules/registration/RegistrationUserController.java
@@ -1,8 +1,10 @@
 package com.zdjavapol110.eventmanager.core.modules.registration;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/api")
 public class RegistrationUserController {
 
 
