425c981c6bd907e75487697d919762a0598eb800
Kasia <kasia@cwiakalska.com>
2022-05-22 10:46:02 +0200

    Basic Spring Setup

---
+++ src/main/java/com/zdjavapol110/eventmanager/EvenTmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.zdjavapol110.eventmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvenTmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvenTmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zdjavapol110/eventmanager/EvenTmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zdjavapol110.eventmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvenTmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
