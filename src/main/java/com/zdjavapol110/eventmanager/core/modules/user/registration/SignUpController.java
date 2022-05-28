bcf39aa5d814c052764e0ec7dff96582dd2149f3
Przemyslaw <brain667@gmail.com>
2022-05-28 17:36:32 +0200

    added message: Invalid username or password

--- src/main/java/com/zdjavapol110/eventmanager/core/modules/user/registration/SignUpController.java
+++ src/main/java/com/zdjavapol110/eventmanager/core/modules/user/registration/SignUpController.java
@@ -1,5 +1,6 @@
 package com.zdjavapol110.eventmanager.core.modules.user.registration;
 
+import com.zdjavapol110.eventmanager.core.modules.event.EventDto;
 import com.zdjavapol110.eventmanager.core.modules.event.EventService;
 import com.zdjavapol110.eventmanager.core.modules.user.repository.UserEntity;
 import com.zdjavapol110.eventmanager.core.modules.user.repository.UserRepository;
@@ -8,9 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -39,17 +45,12 @@ public class SignUpController {
         return "signup/register_success";
     }
 
-//    @GetMapping("/login2")
-//    public String getLogin(Model model) {
-//        model.addAttribute("user", new UserDto());
-//        return "signup/login2.html";
-//    }
-
     @GetMapping("/login")
     public String getLogin(Model model) {
         model.addAttribute("user", new UserDto());
         return "signup/login2.html";
     }
+
     @PostMapping("/process_success")
     public String processEvents(Model model) {
         model.addAttribute("events", eventService.getAllEvents(0, 10));
