03f994248ea483691fc77ba1649e70b720d8c510
Mariusz Maciejewski <maciejewski_mariusz@wp.pl>
2022-05-29 11:34:24 +0200

    Logged user cannot access signup or login page

--- src/main/java/com/zdjavapol110/eventmanager/core/modules/user/registration/SignUpController.java
+++ src/main/java/com/zdjavapol110/eventmanager/core/modules/user/registration/SignUpController.java
@@ -1,68 +1,78 @@
 package com.zdjavapol110.eventmanager.core.modules.user.registration;
 
-import com.zdjavapol110.eventmanager.core.modules.event.EventDto;
 import com.zdjavapol110.eventmanager.core.modules.event.EventService;
 import com.zdjavapol110.eventmanager.core.modules.user.repository.UserEntity;
 import com.zdjavapol110.eventmanager.core.modules.user.repository.UserRepository;
 import com.zdjavapol110.eventmanager.core.modules.user.service.dto.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @Controller
 public class SignUpController {
-    @Autowired
-    private UserRepository userRepository;
+  @Autowired private UserRepository userRepository;
 
-    @Autowired
-    private EventService eventService;
+  @Autowired private EventService eventService;
 
-    @Autowired
-    BCryptPasswordEncoder passwordEncoder;
+  @Autowired BCryptPasswordEncoder passwordEncoder;
 
-    @GetMapping("/signup")
-    public String getSignUp(Model model) {
-        model.addAttribute("signup", new UserDto());
-        model.addAttribute("user", new UserDto());
-        return "signup/signup.html";
+  private boolean isUserLogged() {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
+      return true;
+    } else {
+      return false;
     }
+  }
 
-    @PostMapping("/process_register")
-    public String processRegister(UserEntity user) {
-        String encodedPassword = passwordEncoder.encode(user.getPassword());
-        user.setPassword(encodedPassword);
-        userRepository.save(user);
-        return "signup/register_success";
+  @GetMapping("/signup")
+  public String getSignUp(Model model) {
+    if(isUserLogged()) {
+    model.addAttribute("signup", new UserDto());
+    model.addAttribute("user", new UserDto());
+    return "signup/signup.html"; }
+    else {
+      return "redirect:/";
     }
+  }
 
-    @GetMapping("/login")
-    public String getLogin(Model model) {
-        model.addAttribute("user", new UserDto());
-        return "signup/login2.html";
-    }
+  @PostMapping("/process_register")
+  public String processRegister(UserEntity user) {
+    String encodedPassword = passwordEncoder.encode(user.getPassword());
+    user.setPassword(encodedPassword);
+    userRepository.save(user);
+    return "signup/register_success";
+  }
 
-    @PostMapping("/process_success")
-    public String processEvents(Model model) {
-        model.addAttribute("events", eventService.getAllEvents(0, 10));
-        return "redirect:/events";
-    }
+  @GetMapping("/login")
+  public String getLogin(Model model) {
 
-    @GetMapping("/users")
-    public String listUsers(Model model) {
-        List<UserEntity> listUsers = userRepository.findAll();
-        model.addAttribute("listUsers", listUsers);
-        return "users/users.html";
+    if (isUserLogged()) {
+      model.addAttribute("user", new UserDto());
+      return "signup/login2.html";
+    } else {
+      return "redirect:/";
     }
+  }
+
+  @PostMapping("/process_success")
+  public String processEvents(Model model) {
+    model.addAttribute("events", eventService.getAllEvents(0, 10));
+    return "redirect:/events";
+  }
+
+  @GetMapping("/users")
+  public String listUsers(Model model) {
+    List<UserEntity> listUsers = userRepository.findAll();
+    model.addAttribute("listUsers", listUsers);
+    return "users/users.html";
+  }
 }
-
-
